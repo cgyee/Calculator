ebc5cddfaeb0d495b7555dd89f0a99af68d22bb5
cgyee <cyee2017@fau.edu>
2020-11-15 13:08:29 -0500

    finished conversion from ints to strings for term values

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ let term = {
     "isSet": false,
 };
 
-let firstTerm = 0;
-let secondTerm = 0;
 
+let firstTerm = "";
+let secondTerm = "";
 let op = "";
-
-let total = 0;
 
 function add(a, b) {
     return a + b;
@@ -40,30 +38,36 @@ function operator(a, b, operation) {
             return divide(a, b);
     }
 }
+
+function convertFromStringToInteger(stringNumber) {
+    return parseInt(stringNumber) ? parseInt(stringNumber) : 0;
+}
 //function to set and "add" tens place to term i.e user selects 9 and 9 again gives 9*10+9=99 
-function setTerm(currentTerm, value) {
-    value = parseInt(value);
-    if(checkMaxDigits(currentTerm*10+value)) {
-        currentTerm*=10;
-        currentTerm+= value;
+function updateTermValue(currentTerm, value) {
+    if(checkMaxDigits(currentTerm+value)) {
+        currentTerm+=value;
     }
     else {
         console.log("Over Max Allowed Digits");
     }
     console.log(currentTerm);
-    setDisplay(currentTerm);
+    //setDisplay(currentTerm);
     return currentTerm;
+}
+
+function checkMaxDigits(stringNumber) {
+    return stringNumber.length <= 9 ? true : false;
 }
 
 function setTermValues(e) {
     //if user selects a number save that number to the first term if not set
-    if(term.isSet) {
-        secondTerm = setTerm(secondTerm, e.currentTarget.value);
+    if(firstTerm) {
+        secondTerm = updateTermValue(secondTerm, e.currentTarget.value);
     }
 
     //if user selects a number and the first term is filled save it to the second term
     else {
-        firstTerm = setTerm(firstTerm, e.currentTarget.value);
+        firstTerm = updateTermValue(firstTerm, e.currentTarget.value);
     }
     //console.log(e.currentTarget.value);
 }
@@ -81,11 +85,12 @@ function addOperatorListener(e) {
     //if the term is set assign calculate the total and assign it to the first term
     //and set the second term equal to 0
     if(!op) {op = e.currentTarget.value;}
-    if(term.isSet && secondTerm!=null) {
-        total = operator(firstTerm, secondTerm, op);
-        firstTerm = total;
-        setDisplay(firstTerm);
-        secondTerm = null;
+    if(term.isSet && secondTerm!="") {
+
+        firstTerm = operator(convertFromStringToInteger(firstTerm), convertFromStringToInteger(secondTerm), op);
+        firstTerm = firstTerm.toString();
+        //setDisplay(firstTerm);
+        secondTerm = "";
         console.log(firstTerm);
         op = e.currentTarget.value;
     }
@@ -106,38 +111,19 @@ function setOperatorListener() {
     });
 }
 
-function checkMaxDigits(number) {
-    return number <= 10000000 ? true : false;
-}
-
 function setDisplay(number) {
-    const DIGITS_MAX = 9;
-    let number_of_digits = 1;
     const display = document.querySelector('.interface-display__display');
-    let power = 0;
-    while(display.firstChild) {
-        display.removeChild(display.firstChild);
-    }
-    while(number && number_of_digits < DIGITS_MAX) {
-        const displayNumber = document.createElement('div');
-        const value = number % 10;
-        displayNumber.innerText = value;
-        display.append(displayNumber);
-        number = Math.floor(number/10);
-        number_of_digits+=1;
-    }
 }
 setClickListenerToNumberButtons();
 setOperatorListener();
 const equals = document.querySelector('.interface-keys__keys--equals');
 equals.addEventListener('click', e => {
     if(firstTerm && secondTerm && op) {
-        total= operator(firstTerm, secondTerm, op);
-        firstTerm = total;
-        setDisplay(firstTerm);
-        secondTerm = null;
-        op = null;
-        console.log("="+firstTerm);
+        firstTerm= operator(convertFromStringToInteger(firstTerm), convertFromStringToInteger(secondTerm), op).toString();
+        //setDisplay(firstTerm);
+        secondTerm = "";
+        op = "";
+        console.log("= "+firstTerm);
     }
 
     else if(firstTerm && op) {
