cceb9603ba61a855c064f2861f4041cc87fcca6d
cgyee <cyee2017@fau.edu>
2020-11-16 00:44:04 -0500

    started adding logic for decimal place

--- app.js
+++ app.js
@@ -39,11 +39,11 @@ function operator(a, b, operation) {
     }
 }
 
-
 function convertFromStringToInteger(stringNumber) {
     return parseInt(stringNumber) ? parseInt(stringNumber) : 0;
 }
-//function to set and "add" tens place to term i.e user selects 9 and 9 again gives 9*10+9=99 
+
+//Checks if the currentDigits are less than the max Digits if false appends last user input to currentTerm string and displays result 
 function updateTermValue(currentTerm, value) {
     if(checkMaxDigits(currentTerm+value)) {
         currentTerm+=value;
@@ -60,19 +60,20 @@ function checkMaxDigits(stringNumber) {
     return stringNumber.length <= 9 ? true : false;
 }
 
-function setTermValues(e) {
+//Logic used to handle which value is assigned to which variable/term
+function setTermValues(value) {
     //if user selects a number save that number to the first term if not set
     if(term.isSet) {
-        secondTerm = updateTermValue(secondTerm, e.currentTarget.value);
+        secondTerm = updateTermValue(secondTerm, value);
     }
 
     //if user selects a number and the first term is filled save it to the second term
     else {
-        firstTerm = updateTermValue(firstTerm, e.currentTarget.value);
+        firstTerm = updateTermValue(firstTerm, value);
     }
-    //console.log(e.currentTarget.value);
 }
 
+//Logic to display number on screen, removes the last div if it exists and then appends a new one with result of the users action thus far
 function setDisplay(number) {
     const display = document.querySelector('.interface-display__display');
     while(display.firstChild) {
@@ -80,11 +81,13 @@ function setDisplay(number) {
     }
     const displayNumber = document.createElement('div');
     displayNumber.className = "display__numbers";
+    console.log(number);
     displayNumber.textContent = number;
     display.appendChild(displayNumber);
 
 }
 
+//Clears terms and operator variables
 function clearTermsAndOperator(){
     firstTerm = "";
     secondTerm = "";
@@ -93,8 +96,8 @@ function clearTermsAndOperator(){
     setDisplay(0);
 }
 
+//Logic to handle the arithmetic
 function setOperatorListener(e) {
-   
     //If the operator is not set set it equal to the current event target
     if(!op) {
         op = e.currentTarget.value;
@@ -103,7 +106,6 @@ function setOperatorListener(e) {
     //if the term is set and the secondterm is not an empty string, calculate the total and assign it to the first term
     //and set the second term equal to ""
     if(term.isSet && secondTerm!="") {
-
         firstTerm = operator(convertFromStringToInteger(firstTerm), convertFromStringToInteger(secondTerm), op);
         firstTerm = firstTerm.toString();
         secondTerm = "";
@@ -120,16 +122,20 @@ function setOperatorListener(e) {
     
 }
 
+//Logic to determine the final value of the two terms and operation sign if all are true, else it will do nothing
 function setEqualListener() {
+    //if all values are set do the operation as assigned in op and display the final result
     if(firstTerm && secondTerm && op) {
         firstTerm= operator(convertFromStringToInteger(firstTerm), convertFromStringToInteger(secondTerm), op).toString();
         setDisplay(firstTerm);
+        //Set the secondTerm and op to "" to prevent user from spamming/incorrectly repeating the operation
         secondTerm = "";
         op = "";
         console.log("= "+firstTerm);
     }
 }
 
+//removes the last action the user took from memory if it is a number display 0 for operators an empty string
 function removeLastAction() {
     if(firstTerm && secondTerm && op) {
         secondTerm = "";
@@ -148,6 +154,10 @@ function removeLastAction() {
 
 }
 
+function addDecimalPlace() {
+    setTermValues('.');
+}
+
 function addOperatorListener() {
     let buttons = document.querySelectorAll('.interface-keys__keys--operator');
     Array.from(buttons);
@@ -160,7 +170,7 @@ function addClickListenerNumberButtons() {
     let buttons = document.querySelectorAll('.interface-keys__keys--number');
     buttons = Array.from(buttons);
     buttons.forEach(button => {
-        button.addEventListener('click', setTermValues);
+        button.addEventListener('click', e => setTermValues(e.currentTarget.value));
     });
 }
 
@@ -179,12 +189,18 @@ function addDeleteListener() {
     del.addEventListener('click', removeLastAction);
 }
 
+function addDotListener() {
+    const dot = document.querySelector('.interface-keys__keys--dot');
+    dot.addEventListener('click', addDecimalPlace);
+}
+
 function calculatorSetup() {
     addClickListenerNumberButtons();
     addOperatorListener();
     addEqualEventListener();
     addClearButtonListener();
     addDeleteListener();
+    //addDotListener();
 }
 
 calculatorSetup();
